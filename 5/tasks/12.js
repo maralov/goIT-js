3c8dda4c70a14e64a7fd07e0e27cc77093af343c
maralov <aralov.mykhailo@gmail.com>
2021-03-13 14:05:25 +0200

    autocheck-12

--- 5/tasks/12.js
+++ 5/tasks/12.js
@@ -6,27 +6,32 @@
 */
 
 class Storage {
-  #items;
 
   constructor(items) {
-    this.#items = items;
+    this.items = items;
+  }
+
+  static priority() {
+    return {
+      HIGTH: 'hight'
+    }
   }
 
   getItems() {
-    return this.#items;
+    return this.items;
   }
 
   addItem(newItem) {
-    this.#items.push(newItem);
+    this.items.push(newItem);
   }
 
   removeItem(item) {
-    const itemIndex = this.#items.indexOf(item);
-    this.#items.splice(itemIndex, 1);
+    const itemIndex = this.items.indexOf(item);
+    this.items.splice(itemIndex, 1);
   }
 }
 
-
+console.log(Storage.priority().HIGTH)
 // Пиши код выше этой строки
 const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
 
